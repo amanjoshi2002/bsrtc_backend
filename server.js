2d2d10ac9ebd222f64233ce93f82b7842f2fa5d2
amanjoshi2002 <sctaman21@gmail.com>
2024-09-22 21:55:40 +0530

    updated cors

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const contactInfoRoutes = require('./routes/contactInfoRoutes');
 const divisionRoutes = require('./routes/divisionRoutes');
-
 const contactRoutes = require('./routes/contactInfoRoutes');
 const phoneDirectoryRoutes = require('./routes/phoneDirectoryRoutes');
 const policyRoutes = require('./routes/policyRoutes');
@@ -13,18 +12,18 @@ const popularRouteRoutes = require('./routes/popularRouteRoutes');
 const tenderRoutes = require('./routes/tenderRoutes');
 const galleryRoutes = require('./routes/galleryRoutes');
 const touristDestinationRoutes = require('./routes/touristDestinationRoutes');
-const config = require('config');
+const faqRoutes = require('./routes/faqRoutes');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-const faqRoutes = require('./routes/faqRoutes');
-const app = express();
-
-const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
 connectDB();
 
 // Ensure the uploads directory exists
@@ -33,14 +32,22 @@ if (!fs.existsSync(uploadsDir)) {
     fs.mkdirSync(uploadsDir);
 }
 
-// Serve static files from the uploads directory
-app.use('/uploads', express.static(uploadsDir));
+// CORS configuration
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
 
+// Middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use('/uploads', express.static(uploadsDir));
 
+// Routes
 app.use('/api/contact', contactRoutes);
 app.use('/api/phone-directory', phoneDirectoryRoutes);
 app.use('/api/policies', policyRoutes);
@@ -53,6 +60,14 @@ app.use('/api/gallery', galleryRoutes);
 app.use('/api/tourist-destinations', touristDestinationRoutes);
 app.use('/api/divisions', divisionRoutes);
 app.use('/api/contact-info', contactInfoRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
